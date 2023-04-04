04b9e79f6874ee0c96d9d995e65e0dae6ee0ae55
Carlo <byjuscarlo000@gmail.com>
2023-04-04 17:42:46 -0600

    Inserte un espacio

--- App.js
+++ App.js
@@ -24,6 +24,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        
         <Header
           backgroundColor={'#9c8210'}
           centerComponent={{
